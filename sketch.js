1edf80f744a9f37830ff12435f10c48e31255e75
Giorgia <giorgia.piacentini97@gmail.com>
2018-10-23 18:37:51 +0200

    vediamo

--- sketch.js
+++ sketch.js
@@ -1,11 +1,38 @@
-function preload(){
+var myImage;
+var mysecImage;
+var mySound;
+var analyzer;
+
+function preload() {
   // put preload code here
+  myImage = loadImage('./assets/mood.jpg');
+  mysecImage = loadImage('./assets/volume.png');
+  mySound = loadSound("./assets/waves.mp3");
 }
 
 function setup() {
   // put setup code here
+  createCanvas(windowWidth, windowHeight);
+
+  fft = new p5.FFT();
+  mySound.setVolume(0.5);
+  mySound.play();
+
+
 }
 
 function draw() {
-  // put drawing code here
+  background(lerpColor(color('#B2EBF2'), color('#FFCCBC'), frameCount / 100));
+
+  var a = fft.analyze();
+
+  var b = map(a[256], 50, 500, 1, 40);
+
+  imageMode(CENTER);
+  image(myImage, width / 2, height / 2, myImage.width, myImage.height);
+
+  push();
+  imageMode(CENTER);
+  image(mysecImage, width / 2, height / 2, 80 * 0.4 * b, 40 * 0.4 * b);
+  pop();
 }
